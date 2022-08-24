3da91883061ef81763553d7cf31104251aa13964
DanielNicolasLlanes <daniel.nicolas.llanes>
2022-08-24 02:05:09 -0300

    error de educacion

--- src/main/java/com/porfolioDanielLlanes/demo/Controlador/CEducacion.java
+++ src/main/java/com/porfolioDanielLlanes/demo/Controlador/CEducacion.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/educacion")
+@RequestMapping("educacion")
 @CrossOrigin(origins = "https://porfoliofronted.web.app")
 public class CEducacion {
 
